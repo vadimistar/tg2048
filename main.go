cc4034724698cdba6ae1771fb39d60cc67a91a28
Vadimistar <vadimistar@ya.ru>
2022-07-06 14:30:44 +0700

    Telegram bot works for a single user

--- main.go
+++ main.go
@@ -6,37 +6,43 @@ import (
 	"math/rand"
 	"os"
 	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 const fieldSize = 4
 const startSquareVal = 2
 
-var field = make([][]int, fieldSize)
-var score = 0
+type field [][]int
 
-func showField() {
+func newField() field {
+	f := make(field, fieldSize)
+	for i := 0; i < fieldSize; i++ {
+		f[i] = make([]int, fieldSize)
+	}
+	return f
+}
+
+func (f field) String() string {
+	res := "```\n"
 	for i := 0; i < fieldSize; i++ {
 		for j := 0; j < fieldSize; j++ {
-			if field[i][j] != 0 {
-				fmt.Printf("|%5d|", field[i][j])
+			if f[i][j] != 0 {
+				res += fmt.Sprintf("[%4d]\t", f[i][j])
 			} else {
-				fmt.Print("|     |")
+				res += "[    ]\t"
 			}
 		}
-		fmt.Println()
+		res += "\n"
 	}
+	res += "```\n"
+	return res
 }
 
-type GameOver struct{}
-
-func (g GameOver) Error() string {
-	return "Game over"
-}
-
-func fieldIsFull() bool {
+func (f field) full() bool {
 	for i := 0; i < fieldSize; i++ {
 		for j := 0; j < fieldSize; j++ {
-			if field[i][j] == 0 {
+			if f[i][j] == 0 {
 				return false
 			}
 		}
@@ -44,16 +50,37 @@ func fieldIsFull() bool {
 	return true
 }
 
-func checkOverlap(x *int, y *int) error {
-	if fieldIsFull() {
-		return GameOver{}
+type game struct {
+	field
+	score int
+}
+
+func newGame() game {
+	return game{
+		field: newField(),
+	}
+}
+
+func (g game) String() string {
+	return fmt.Sprintf("Score: %d\n%s", g.score, g.field)
+}
+
+type gameOver struct{}
+
+func (g gameOver) Error() string {
+	return "Game over"
+}
+
+func (f field) checkOverlap(x *int, y *int) error {
+	if f.full() {
+		return gameOver{}
 	}
 	for {
-		if field[*x][*y] != 0 {
-			if *x == fieldSize-1 {
-				*y = (*y + 1) % fieldSize
+		if f[*x][*y] != 0 {
+			if *x == len(f)-1 {
+				*y = (*y + 1) % len(f)
 			}
-			*x = (*x + 1) % fieldSize
+			*x = (*x + 1) % len(f)
 			continue
 		}
 		return nil
@@ -63,61 +90,61 @@ func checkOverlap(x *int, y *int) error {
 var rngSrc = rand.NewSource(time.Now().UnixNano())
 var rng = rand.New(rngSrc)
 
-func prepare() error {
-	if _, _, e := addSquare(); e != nil {
+func (f field) prepare() error {
+	if _, _, e := f.addSquare(); e != nil {
 		return e
 	}
-	if _, _, e := addSquare(); e != nil {
+	if _, _, e := f.addSquare(); e != nil {
 		return e
 	}
 	return nil
 }
 
-func addSquare() (int, int, error) {
+func (f field) addSquare() (int, int, error) {
 	x := rng.Intn(fieldSize)
 	y := rng.Intn(fieldSize)
-	if e := checkOverlap(&x, &y); e != nil {
+	if e := f.checkOverlap(&x, &y); e != nil {
 		return 0, 0, e
 	}
-	field[x][y] = startSquareVal
+	f[x][y] = startSquareVal
 	return x, y, nil
 }
 
-func handle(b byte) bool {
+func (g *game) handle(b byte) bool {
 	switch b {
 	case 'a':
-		left()
+		g.left()
 	case 'w':
-		up()
+		g.up()
 	case 's':
-		down()
+		g.down()
 	case 'd':
-		right()
+		g.right()
 	default:
 		return false
 	}
 	return true
 }
 
-func move(x int, y int, next func(int, int) (int, int), check func(int, int) bool, incr func(*int, *int)) {
+func (g *game) move(x int, y int, next func(int, int) (int, int), check func(int, int) bool, incr func(*int, *int)) {
 	for ; check(x, y); incr(&x, &y) {
 		nx, ny := next(x, y)
-		if field[nx][ny] == 0 {
-			field[nx][ny] = field[x][y]
+		if g.field[nx][ny] == 0 {
+			g.field[nx][ny] = g.field[x][y]
 			log.Printf("%d %d is moved from %d %d", nx, ny, x, y)
-		} else if field[nx][ny] == field[x][y] {
-			field[nx][ny] += field[x][y]
-			score += field[nx][ny]
+		} else if g.field[nx][ny] == g.field[x][y] {
+			g.field[nx][ny] += g.field[x][y]
+			g.score += g.field[nx][ny]
 			log.Printf("%d %d combines with %d %d", nx, ny, x, y)
 		} else {
 			break
 		}
-		field[x][y] = 0
+		g.field[x][y] = 0
 	}
 }
 
-func moveLeft(x int, y int) {
-	move(x, y, func(x, y int) (int, int) {
+func (g *game) moveLeft(x int, y int) {
+	g.move(x, y, func(x, y int) (int, int) {
 		return x, y - 1
 	}, func(_, y int) bool {
 		return y > 0
@@ -126,8 +153,8 @@ func moveLeft(x int, y int) {
 	})
 }
 
-func moveUp(x int, y int) {
-	move(x, y, func(x, y int) (int, int) {
+func (g *game) moveUp(x int, y int) {
+	g.move(x, y, func(x, y int) (int, int) {
 		return x - 1, y
 	}, func(x, _ int) bool {
 		return x > 0
@@ -136,8 +163,8 @@ func moveUp(x int, y int) {
 	})
 }
 
-func moveDown(x int, y int) {
-	move(x, y, func(x, y int) (int, int) {
+func (g *game) moveDown(x int, y int) {
+	g.move(x, y, func(x, y int) (int, int) {
 		return x + 1, y
 	}, func(x, _ int) bool {
 		return x < fieldSize-1
@@ -146,8 +173,8 @@ func moveDown(x int, y int) {
 	})
 }
 
-func moveRight(x int, y int) {
-	move(x, y, func(x, y int) (int, int) {
+func (g *game) moveRight(x int, y int) {
+	g.move(x, y, func(x, y int) (int, int) {
 		return x, y + 1
 	}, func(_, y int) bool {
 		return y < fieldSize-1
@@ -156,63 +183,150 @@ func moveRight(x int, y int) {
 	})
 }
 
-func left() {
+func (g *game) left() {
 	for x := 0; x < fieldSize; x++ {
 		for y := 0; y < fieldSize; y++ {
-			if field[x][y] != 0 {
-				moveLeft(x, y)
+			if g.field[x][y] != 0 {
+				g.moveLeft(x, y)
 			}
 		}
 	}
 }
 
-func up() {
+func (g *game) up() {
 	for x := 0; x < fieldSize; x++ {
 		for y := 0; y < fieldSize; y++ {
-			if field[x][y] != 0 {
-				moveUp(x, y)
+			if g.field[x][y] != 0 {
+				g.moveUp(x, y)
 			}
 		}
 	}
 }
 
-func down() {
+func (g *game) down() {
 	for x := fieldSize - 1; x >= 0; x-- {
 		for y := 0; y < fieldSize; y++ {
-			if field[x][y] != 0 {
-				moveDown(x, y)
+			if g.field[x][y] != 0 {
+				g.moveDown(x, y)
 			}
 		}
 	}
 }
 
-func right() {
+func (g *game) right() {
 	for x := 0; x < fieldSize; x++ {
 		for y := fieldSize - 1; y >= 0; y-- {
-			if field[x][y] != 0 {
-				moveRight(x, y)
+			if g.field[x][y] != 0 {
+				g.moveRight(x, y)
 			}
 		}
 	}
 }
 
+const updateConfigTimeout = 30
+
+var gameKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("⬅️️"),
+		tgbotapi.NewKeyboardButton("️⬆️"),
+		tgbotapi.NewKeyboardButton("️➡️️️"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton(" "),
+		tgbotapi.NewKeyboardButton("⬇️"),
+		tgbotapi.NewKeyboardButton(" "),
+	),
+)
+
 func main() {
-	for i := 0; i < fieldSize; i++ {
-		field[i] = make([]int, fieldSize)
+	log.Println("Token is", os.Getenv("TELEGRAM_API_TOKEN"))
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
+	if err != nil {
+		log.Fatalln("Can't create Telegram API:", err, "(API token might be invalid)")
 	}
-	prepare()
-	b := make([]byte, 256)
-	for {
-		fmt.Println("Score: ", score)
-		showField()
-		os.Stdin.Read(b)
-		if handle(b[0]) {
-			log.Println(string(b[0]), "was handled")
-			x, y, err := addSquare()
-			if err != nil {
-				panic(err)
-			}
-			log.Printf("square at %d %d was generated", x, y)
+	bot.Debug = true
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = updateConfigTimeout
+	updates := bot.GetUpdatesChan(updateConfig)
+	game := newGame()
+	if err := game.prepare(); err != nil {
+		log.Fatalln(err)
+	}
+	lastMsgId := 0
+	for update := range updates {
+		if update.Message == nil {
+			continue
 		}
+		if lastMsgId == 0 {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, `**tg2048**
+2048 Clone Telegram Bot
+by [vadimistar](https://github.com/vadimistar) *(Vadim Starostin)*`)
+			msg.ParseMode = "Markdown"
+			msg.ReplyMarkup = gameKeyboard
+			if _, err := bot.Send(msg); err != nil {
+				log.Fatalln(err)
+			}
+		} else {
+			bot.Send(tgbotapi.NewDeleteMessage(update.Message.Chat.ID,
+				lastMsgId))
+		}
+		input := update.Message.Text
+		d := tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID)
+		bot.Send(d)
+		switch input {
+		case "⬅️️":
+			game.left()
+		case "️⬆️":
+			game.up()
+		case "️➡️️️":
+			game.right()
+		case "⬇️":
+			game.down()
+		default:
+			continue
+		}
+		x, y, err := game.addSquare()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("square at %d %d was generated", x, y)
+		fieldMsg := tgbotapi.NewMessage(update.Message.Chat.ID, game.String())
+		fieldMsg.ParseMode = "Markdown"
+		sentFieldMsg, err := bot.Send(fieldMsg)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		lastMsgId = sentFieldMsg.MessageID
+		//switch update.Message.Command() {
+		// case "start":
+		// 	msg.ReplyMarkup = gameKeyboard
+		// default:
+		// 	msg.Text = update.Message.Command()
+		// }
+		// if _, err := bot.Send(msg); err != nil {
+		// 	log.Fatalln(err)
+		// }
+		// if game.handle(update.Message.Text[0]) {
+		// 	msg := tgbotapi.NewMessage()
+		// }
+		// msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		// msg.ReplyToMessageID = update.Message.MessageID
+		// if _, err := bot.Send(msg); err != nil {
+		// 	log.Fatalln(err)
+		// }
 	}
+	// b := make([]byte, 256)
+	// for {
+	// 	fmt.Println("Score: ", score)
+	// 	showField()
+	// 	os.Stdin.Read(b)
+	// 	if handle(b[0]) {
+	// 		log.Println(string(b[0]), "was handled")
+	// 		x, y, err := addSquare()
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		log.Printf("square at %d %d was generated", x, y)
+	// 	}
+	// }
 }
